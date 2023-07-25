9958e70fe20716558eb025d94b95e7140af7a24a
ERIKA ANDREA PAZ QUINTEROS <00117420@uca.edu.sv>
2023-07-25 14:46:14 -0600

    :sparkles: Add controlled input

--- components/Ingredients.jsx
+++ components/Ingredients.jsx
@@ -1,13 +1,18 @@
+"use client"
+
 import DomPurify from "isomorphic-dompurify"
+import { useState } from "react"
 
 export function Ingredients({ title}) {
+    const [isActive, setIsActive] = useState(false)
+
     return (
         <div className="flex items-start gap-2 pb-3 xl:gap-4">
             <div className="min-w-[21px] h-[21px]">
-                <input type="checkbox" id={title} name={title} value={title} className=" border-[#828282] accent-[#F2994A] w-full h-full object-cover"/>
+                <input id={title} type="checkbox" className="border-[#828282] accent-[#F2994A] w-full h-full object-cover" checked={isActive} onChange={e => setIsActive(e.target.checked)}/>
             </div>
 
-            <p className="font-normal text-sm text-[#333333] leading-6 xl:text-base xl:w-[542px] [&>span]:font-bold" dangerouslySetInnerHTML={{__html: DomPurify.sanitize(title)}}/>
+            <label htmlFor={title} className={`font-normal text-sm text-[#333333] leading-6 xl:text-base xl:w-[542px] [&>span]:font-bold ${isActive && "line-through"} `} dangerouslySetInnerHTML={{__html: DomPurify.sanitize(title)}}/>
         </div>
     )
 }
